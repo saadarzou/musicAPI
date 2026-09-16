e5382bbc4b29df3dd333eca021007953769f2052
saadarzou <saadarzou62@gmail.com>
2026-09-16 11:44:33 +0200

    xx

--- api.php
+++ api.php
@@ -1,35 +1,42 @@
   <?php
 
-include("connection.php");
+include("config/connection.php");
+var_dump($conn);
 
 $genereScelto="tutte";
 
+// vediamo se e stato scelto un genere
 if(isset($_GET["genere"])){
-    $genereScelto=$_GET["genere"];
+
+    // se e stato scelto un genere lo salviamo all'interno del parametro genereScelto
+    $genereScelto = $_GET["genere"];
 
 }
 
 $stessoGenere = [];
 
-for($i=0;$i < count($dischi); $i++){
+if($genereScelto == "tutte"){
+    // se e sato scelto tutte prerndiamo tutti i dischi del databse
+$query = "SELECT * FROM dischi";
+$risultatoQuery = mysqli_query($conn, $query);
+} else {
+    // prendiamo solo i dischi che hanno lo stesso genere
+    $query = "SELECT * FROM dischi WHERE genere = '$genereScelto'";
+    // fa la richiesta sul database e salva i dati all'interno di risultatoQuery
+    $risultatoQuery = mysqli_query($conn, $query);
+}
 
-    if($genereScelto=="tutte"){
-        $stessoGenere[] = $dischi[$i];
-    } elseif ($genereScelto  == $dischi[$i]["genere"]){
-        $stessoGenere[] = $dischi[$i];
-    }
+//prendiamo i dati dal database
+while($disco = mysqli_fetch_assoc($risultatoQuery)){
+
+    $stessoGenere[] = $disco;
 
 }
 
-
+//trasformiamo l'array in file json
 header("Content-Type: application/json");
-$risultato=json_encode($stessoGenere);
-echo $risultato;
 
-
-
-
-
+echo json_encode($stessoGenere);
 
 
 
